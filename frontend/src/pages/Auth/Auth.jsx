781e33abeebd3baa14f63cc7b4dd2a95446356ea
Balreddy mahendra <balreddy.mahendra@dokonaly.com>
2022-12-13 16:52:55 +0530

    everything done expect input specific validations

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { logInHandler } from "./AuthSlice";
 import { useNavigate } from "react-router";
+import { useHttpClient } from "../../core/store/hooks/htttp-hook";
+import Loader from "../../components/Loader/Loader";
 import "./Auth.css";
 
-
 export const Auth = () => {
+  const { apiCall, isLoading } = useHttpClient();
+
   const [name, setName] = useState("");
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(false);
-  const [isValid,setIsValid] = useState(true);
-
 
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+ 
 
+  const resetInputHandler = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+    setUserName("");
+  }
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -30,101 +38,96 @@ export const Auth = () => {
     };
     if (isLogin) {
       let userData = { email, password };
+      
+        dispatch(logInHandler(userData))
+          .unwrap()
+          .then((data) => {
+            navigate("/dashboard");
+          })
+          .catch((err) => {
+            alert(err.error);
+          });
+     
+    } else {
       try {
-        dispatch(logInHandler(userData)).unwrap().then((data) => {
-        navigate("/dashboard")
-        console.log(data,"data in login")
-       }).catch((err) => {
-        alert(err.error)
-       });
-       
+        const response = await apiCall(
+          "//localhost:4000/api/signup",
+          "POST",
+          JSON.stringify(userDetails),
+          {
+            "Content-Type": "application/json",
+          }
+        );
+
+        if (response.error) {
+          alert(response.error);
+        } else {
+          setIsLogin(!isLogin);
+        }
       } catch (err) {
         console.log(err);
       }
-
-    } else {
-
-    
-      try{
-        const response = await fetch("//localhost:4000/api/signup", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(userDetails),
-        });
-        const data = await response.json();
-        
-        if (data.error) {
-          alert(data.error);
-        }else{
-          setIsLogin(!isLogin)
-        }
-        
-      }catch(err){
-        console.log(err)
-      }
-     
     }
-    console.log(userDetails, "user details");
-    setName("");
-    setEmail("");
-    setPassword("");
-    setUserName("");
+    
+    resetInputHandler()
+
   };
-
-
 
   const switchModeHandler = () => {
     setIsLogin(!isLogin);
   };
-  
+
   return (
     <div className="auth-section">
-      <form onSubmit={submitHandler}>
-        <h2>{isLogin ? "Login to App" : "Register to App"}</h2>
-        {!isLogin && <label>Fullname</label>}
-        {!isLogin && 
-        
+      {isLoading ? (
+        <Loader asOverlay />
+      ) : (
+        <form onSubmit={submitHandler}>
+          <h2>{isLogin ? "Login to App" : "Register to App"}</h2>
+          {!isLogin && <label>Fullname</label>}
+          {!isLogin && (
+            <input required
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              value={name}
+            />
+          )}
+          {!isLogin && <label>Username</label>}
+          {!isLogin && (
+            <input
+              required
+              onChange={(e) => setUserName(e.target.value)}
+              type="text"
+              value={userName}
+            />
+          )}
+          <label>Email</label>
           <input
-            onChange={(e) => setName(e.target.value)}
-            type="text"
-            value={name}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            value={email}
           />
-
-       
-        }
-        {!isLogin && <label>Username</label>}
-        {!isLogin && (
+          <label>Password</label>
           <input
-            onChange={(e) => setUserName(e.target.value)}
+            required
             type="text"
-            value={userName}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
-        )}
-        <label>Email</label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          value={email}
-        />
-        <label>Password</label>
-        <input
-          type="text"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <button type="submit" disabled={!isValid}>
-          Submit
-        </button>
-        <div className="flex-container">
-          <p>{isLogin ? "New user Please ,  " : "Already Register Please,"}</p>
-          <a href="#" onClick={switchModeHandler}>
-            {isLogin ? "Register" : "Login"}
-          </a>
-        </div>
-        {/* <p>Already Register Pease </p> */}
-      </form>
+          {/* <div className="error">{password.length > 5 ? "": "Minium six characters required"}</div> */}
+          <button type="submit" disabled={isLogin ? !email || !password : !email || !password || !name || !userName}>
+            Submit
+          </button>
+          <div className="flex-container">
+            <p>
+              {isLogin ? "New user Please ,  " : "Already Register Please,"}
+            </p>
+            <a href="#" onClick={switchModeHandler}>
+              {isLogin ? "Register" : "Login"}
+            </a>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
